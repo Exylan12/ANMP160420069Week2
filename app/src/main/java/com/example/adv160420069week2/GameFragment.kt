305161a1bd53a2e241766832ae4fe64a7cbc2bd9
Exylan12 <kristiankansil098@gmail.com>
2023-03-08 11:56:10 +0700

    ANMP Week2(Class Tutorial)

---
+++ app/src/main/java/com/example/adv160420069week2/GameFragment.kt
@@ -0,0 +1,40 @@
+package com.example.adv160420069week2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(arguments != null) {
+            val playerName =
+                GameFragmentArgs.fromBundle(requireArguments()).playerName
+            val txtTurn = view.findViewById<TextView>(R.id.txtTurn)
+            txtTurn.text = "$playerName's Turn"
+        }
+        val txtNumber1 = view.findViewById<TextView>(R.id.txtNumber1)
+        val txtNumber2 = view.findViewById<TextView>(R.id.txtNumber2)
+
+    }
+
+}
